caa79a13c6e1b125fade108e5366567f5d847e92
EthanWalton-snlm <ethan.walton@sanlam.co.za>
2025-07-27 12:16:43 +0200

    Vehicle list grid; temporary background

--- src/pages/Vehicles/Vehicles.jsx
+++ src/pages/Vehicles/Vehicles.jsx
@@ -1,13 +1,16 @@
+import "./styles.css";
 import { Vehicle } from "../../components/Vehicle/Vehicle";
 
 function Vehicles({ vehicleData, setVehicleData }) {
   // console.log(vehicleData);
   return (
     <>
-      <Vehicle vehicle={vehicleData[0]} />
-      {/* {vehicleData.map((vehicle) => {
-        <Vehicle vehicle={vehicle} />;
-      })} */}
+      {/* <Vehicle vehicle={vehicleData[0]} /> */}
+      <div className="vehicles-container">
+        {vehicleData.map((vehicle) => (
+          <Vehicle vehicle={vehicle} />
+        ))}
+      </div>
     </>
   );
 }
